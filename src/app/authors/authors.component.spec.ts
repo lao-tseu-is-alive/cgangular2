fbf171d8a91d5620bd669549c677f4a4095011e1
lao.tseu.is.alive@gmail.com <lao.tseu.is.alive@gmail.com>
2016-09-13 17:37:27 +0200

    update 2 way data binding

--- src/app/authors/authors.component.spec.ts
+++ src/app/authors/authors.component.spec.ts
@@ -4,10 +4,12 @@ import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { addProviders, async, inject } from '@angular/core/testing';
 import { AuthorsComponent } from './authors.component';
+import {AuthorsService} from './authors.service';
 
 describe('Component: Authors', () => {
   it('should create an instance', () => {
-    let component = new AuthorsComponent();
+    let service = new AuthorsService();
+    let component = new AuthorsComponent(service);
     expect(component).toBeTruthy();
   });
 });
