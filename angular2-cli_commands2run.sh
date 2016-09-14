fe67baa33675830ae2b560978f1f82edf73c5ce4
lao.tseu.is.alive@gmail.com <lao.tseu.is.alive@gmail.com>
2016-09-14 10:11:50 +0200

    reconciliation

---
+++ angular2-cli_commands2run.sh
@@ -0,0 +1,6 @@
+echo "this angular-cli commands where done to help generates scaffold code"
+ng g component courses
+ng g service courses
+ng g directive auto-grow
+ng g component authors
+ng g service authors
